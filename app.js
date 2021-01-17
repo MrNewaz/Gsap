f93a9356be0408c69031ccdfc6ecdddb1084df38
MrNewaz <saifur.saif@northsouth.edu>
2021-01-17 17:54:15 +0600

    init

--- app.js
+++ app.js
@@ -5,67 +5,64 @@ let pageScene;
 function animateSlides() {
   //Init Controller
   controller = new ScrollMagic.Controller();
-  //Select Some things
+  //Select some things
   const sliders = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
-  //lopp over slides
+  //Loop over each sllide
   sliders.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
-    //Gsap
+    //GSAP
     const slideTl = gsap.timeline({
       defaults: { duration: 1, ease: "power2.inOut" },
     });
     slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.75");
-    //Scene
+    //Create Scene
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
       triggerHook: 0.25,
       reverse: false,
     })
       .setTween(slideTl)
-      .addIndicators({
-        colorStart: "White",
-        colorTrigger: "white",
-        name: "slide",
-      })
+      // .addIndicators({
+      //   colorStart: "white",
+      //   colorTrigger: "white",
+      //   name: "slide"
+      // })
       .addTo(controller);
-
-    //New animation
+    //New ANimation
     const pageTl = gsap.timeline();
     let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
     pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
     pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
     pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
-
-    //Create New Scene
+    //Create new scene
     pageScene = new ScrollMagic.Scene({
       triggerElement: slide,
       duration: "100%",
       triggerHook: 0,
     })
+      // .addIndicators({
+      //   colorStart: "white",
+      //   colorTrigger: "white",
+      //   name: "page",
+      //   indent: 200
+      // })
       .setPin(slide, { pushFollowers: false })
       .setTween(pageTl)
-      .addIndicators({
-        colorStart: "White",
-        colorTrigger: "white",
-        name: "page",
-        indent: 200,
-      })
       .addTo(controller);
   });
 }
-let mouse = document.querySelector(".cursor");
-let mouseText = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
   mouse.style.left = e.pageX + "px";
 }
-
 function activeCursor(e) {
   const item = e.target;
   if (item.id === "logo" || item.classList.contains("burger")) {
@@ -76,14 +73,34 @@ function activeCursor(e) {
   if (item.classList.contains("explore")) {
     mouse.classList.add("explore-active");
     gsap.to(".title-swipe", 1, { y: "0%" });
-    mouseText.innerText = "Tap";
+    mouseTxt.innerText = "Tap";
   } else {
     mouse.classList.remove("explore-active");
+    mouseTxt.innerText = "";
     gsap.to(".title-swipe", 1, { y: "100%" });
-    mouseText.innerText = "";
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 3, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -3, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(0px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+
+//Event Listeners
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 
